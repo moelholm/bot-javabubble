c9abd06e760196565ac657f0f5f180c39dd8fef6
Nicky Moelholm <moelholm@gmail.com>
2022-12-27 14:45:31 +0100

    initial commit 3875 (#18)

--- iac-javabubble/bin/iac-javabubble.ts
+++ iac-javabubble/bin/iac-javabubble.ts
@@ -2,6 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { IacJavabubbleStack } from "../lib/iac-javabubble-stack";
+import { IacAwsbubbleStack } from "../lib/iac-awsbubble-stack";
 
 const app = new cdk.App();
 new IacJavabubbleStack(app, "IacJavabubbleStack", {});
+new IacAwsbubbleStack(app, "IacAwsbubbleStack", {});
---
+++ iac-javabubble/lib/iac-awsbubble-stack.ts
@@ -0,0 +1,93 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { BillingMode } from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+
+export class IacAwsbubbleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    //
+    // DynamoDB table and Global Secondary Index (GSI)
+    //
+    const table = new dynamodb.Table(this, "awsbubbletable", {
+      partitionKey: {
+        name: "FediverseHandle",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+    const gsiName = "LastAnnouncedDateTimeEpoch-index";
+    table.addGlobalSecondaryIndex({
+      indexName: gsiName,
+      partitionKey: {
+        name: "ItemSource",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "LastAnnouncedDateTimeEpoch",
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      projectionType: dynamodb.ProjectionType.INCLUDE,
+      nonKeyAttributes: ["FediverseHandle", "LastAnnouncedDateTime"],
+    });
+    //
+    // Function: Wakes up and toots about 'new' accounts
+    //
+    const announceNewAccountsLambdaFunction = this.createFunction(
+      "awsbubblebotfunction",
+      "build/announce-new-accounts-service.announceNewAccounts",
+      table,
+      gsiName
+    );
+    const newAccountsRule = new events.Rule(this, "awsbubblebotrule", {
+      schedule: events.Schedule.cron({ minute: "30", hour: "*/2" }),
+      targets: [new targets.LambdaFunction(announceNewAccountsLambdaFunction)],
+    });
+    //
+    // Function: Wakes up and toots about 'old' accounts
+    //
+    const announceOldAccountsLambdaFunction = this.createFunction(
+      "awsbubblebotfunctionold",
+      "build/announce-old-accounts-service.announceOldAccounts",
+      table,
+      gsiName
+    );
+    const oldAccountsRule = new events.Rule(this, "awsbubblebotruleold", {
+      schedule: events.Schedule.cron({ hour: "10", minute: "10" }),
+      targets: [new targets.LambdaFunction(announceOldAccountsLambdaFunction)],
+    });
+  }
+
+  createFunction(
+    id: string,
+    handler: string,
+    table: dynamodb.Table,
+    gsiName: string
+  ) {
+    const lambdaFunction = new lambda.Function(this, id, {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: handler,
+      timeout: cdk.Duration.seconds(45),
+      code: lambda.Code.fromAsset("../app-javabubble/lambda-function.zip"),
+      environment: {
+        SSM_PATH: "/APPLICATION/BOT-AWSBUBBLE",
+        DDB_TABLE: table.tableName,
+        DDB_LAST_ANNOUNCED_INDEX: gsiName,
+      },
+    });
+    lambdaFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["ssm:GetParametersByPath"],
+        resources: [
+          `arn:aws:ssm:${this.region}:${this.account}:parameter/APPLICATION/BOT-AWSBUBBLE`,
+        ],
+      })
+    );
+    table.grantReadWriteData(lambdaFunction);
+    return lambdaFunction;
+  }
+}
